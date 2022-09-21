2bf6d4d6ddb590e7b6a24839cd9d9612f0b900db
JonasKaruzas <jonas.karuzas@gmail.com>
2022-09-21 21:16:09 +0300

    feat: :sparkles: getWeather

--- script.js
+++ script.js
@@ -4,9 +4,24 @@
 // show weather predicion
 // show clothes recomendation
 
-const LOCATION_API = "http://ip-api.com/json/?fields=status,country,city";
+const LOCATION_API = "http://ip-api.com/json/?fields=status,city";
 
 async function getLocation() {
   const response = await fetch(LOCATION_API);
   return response.json();
 }
+
+async function getWeather(location) {
+  const response = await fetch(
+    `https://weatherdbi.herokuapp.com/data/weather/${location}`
+  );
+  return response.json();
+}
+
+async function runProgram() {
+  const location = await getLocation();
+  const weatherObj = await getWeather(location.city);
+  console.log(weatherObj.next_days);
+}
+
+runProgram();
