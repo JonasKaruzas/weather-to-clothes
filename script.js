e26bfce53956135bdcc8ff6417435c8bf30e8f4c
JonasKaruzas <jonas.karuzas@gmail.com>
2022-09-21 20:47:15 +0300

    feat: :sparkles: get location from IP

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// Time to script!
+// Get user location
+// Get weather info
+// get user options (man , woman, child)
+// show weather predicion
+// show clothes recomendation
+
+const LOCATION_API = "http://ip-api.com/json/?fields=status,country,city";
+
+async function getLocation() {
+  const response = await fetch(LOCATION_API);
+  return response.json();
+}
